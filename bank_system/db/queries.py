2486a2011c01ef0cbd49b5a722ac1bbdbd243ff2
Pedram <pedisdi@gmail.com>
2024-05-15 11:52:48 +0330

    Add transaction save to BankAccount class's method

--- bank_system/db/queries.py
+++ bank_system/db/queries.py
@@ -15,7 +15,7 @@ CREATE_TABLE_TRANSACTION = (f"CREATE TABLE IF NOT EXISTS transactions ("
                             f"transaction_id SERIAL PRIMARY KEY,"
                             f"transaction_type VARCHAR(10) CHECK(transaction_type IN {TRANSACTIONS}),"
                             f"amount DECIMAL(10, 2) NOT NULL,"
-                            f"timestamp DATE NOT NULL,"
+                            f"date_time TIMESTAMP NOT NULL,"
                             f"account_id INT NOT NULL,"
                             f"transaction_id_from INT,"
                             f"FOREIGN KEY (transaction_id_from) REFERENCES transactions(transaction_id))")
@@ -24,6 +24,8 @@ CREATE_TABLE_TRANSACTION = (f"CREATE TABLE IF NOT EXISTS transactions ("
 # insert queries
 REGISTER_NEW_USER = "INSERT INTO users (username, password) VALUES (%s, %s)"
 CREATE_NEW_ACCOUNT = "INSERT INTO accounts (balance, user_id) VALUES (%s, %s)"
+SAVE_TRANSACTION = ("INSERT INTO transactions (transaction_type, amount, date_time, account_id, transaction_id_from) "
+                    "VALUES (%s, %s, %s, %s, %s)")
 
 # update queries
 UPDATE_ACCOUNT_BALANCE = "UPDATE accounts SET balance = %s WHERE account_id = %s"
--- bank_system/models/bank_account.py
+++ bank_system/models/bank_account.py
@@ -1,3 +1,5 @@
+import datetime
+
 from psycopg2 import extras
 from ..db import db_manager
 from ..db import queries as q
@@ -9,15 +11,21 @@ class BankAccount:
         self.balance = balance  # TODO: balance should be a positive float / minimum balance
         self.user_id = user_id  # user_id or username?
 
+    # TODO: add return result_message to functions
     def create_account(self, cursor):
         """Insert this account to accounts table in database"""
         cursor.execute(q.CREATE_NEW_ACCOUNT, (self.balance, self.user_id))
+
+    @staticmethod
+    def timestamp():
+        return datetime.datetime.now()
 
     # TODO: amount must be positive float
     @staticmethod
     def deposit(account_id, current_balance, amount, cursor):
         new_balance = current_balance + amount
         cursor.execute(q.UPDATE_ACCOUNT_BALANCE, (new_balance, account_id))
+        cursor.execute(q.SAVE_TRANSACTION, ('deposit', amount, BankAccount.timestamp(), account_id, None))
 
     @staticmethod
     def withdraw(account_id, current_balance, amount, cursor):
@@ -26,9 +34,11 @@ class BankAccount:
         if new_balance < 0:
             raise ValueError(m.Messages.NOT_ENOUGH_BALANCE_ERROR)
         cursor.execute(q.UPDATE_ACCOUNT_BALANCE, (new_balance, account_id))
+        cursor.execute(q.SAVE_TRANSACTION, ('withdrawal', -amount, BankAccount.timestamp(), account_id, None))
 
     @staticmethod
     def transfer(account_id_from, account_id_to, current_balance_from, current_balance_to, amount, cursor):
+        # TODO: save the transaction
         """:raise not enough balance"""
         new_balance_from = current_balance_from - amount
         if new_balance_from < 0:
@@ -39,7 +49,6 @@ class BankAccount:
         cursor.execute(q.NEXT_TRANSACTION_ID)
         transaction_id_from = cursor.fetchone()
         print(type(transaction_id_from), transaction_id_from)
-        # TODO: save the transaction
 
 
 if __name__ == "__main__":
@@ -47,13 +56,13 @@ if __name__ == "__main__":
     with db_manager.DBManager() as db:
         # account.create_account(db.cursor)
         # BankAccount.deposit(1, 50.2, 100, db.cursor)
-        # BankAccount.withdraw(1, 50.2, 100, db.cursor)
+        BankAccount.withdraw(1, 52.2, 10, db.cursor)
         # BankAccount.withdraw(1, 150.2, 100, db.cursor)
-        BankAccount.transfer(
-            account_id_from=1,
-            account_id_to=2,
-            current_balance_from=50.2,
-            current_balance_to=50.2,
-            amount=2,
-            cursor=db.cursor
-        )
+        # BankAccount.transfer(
+        #     account_id_from=1,
+        #     account_id_to=2,
+        #     current_balance_from=50.2,
+        #     current_balance_to=50.2,
+        #     amount=2,
+        #     cursor=db.cursor
+        # )
