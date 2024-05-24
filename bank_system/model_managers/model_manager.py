3cd3e464c7702a930a6e58ff4497a43d9a77cdcc
Pedram <pedisdi@gmail.com>
2024-05-24 13:38:39 +0330

    Enhance ModelManager calss

--- bank_system/model_managers/model_manager.py
+++ bank_system/model_managers/model_manager.py
@@ -9,10 +9,25 @@ class ModelManager:
         self.table_name = table_name
         self.model_class = model_class
 
+    def _generate_query_from_kwargs(self, **kwargs):
+        query = q.ALL.format(self.table_name)
+        params = []
+        if kwargs:
+            query += q.WHERE
+            query += q.AND.join([q.BY_COLUMN.format(column) for column in kwargs.keys()])
+            params = tuple(kwargs.values())
+        return query, params
+
+    def _execute_query_from_kwargs(self, cursor, **kwargs):
+        query, params = self._generate_query_from_kwargs(**kwargs)
+        cursor.execute(query, params)
+
     def get(self, **kwargs):
-        model_objects = self.filter(**kwargs)
-        if model_objects:
-            return model_objects[0]
+        with self.db_manager as cursor:
+            self._execute_query_from_kwargs(cursor, **kwargs)
+            record = cursor.fetchone()
+            if record:
+                return self.model_class(**record)
             # TODO: raise a proper Error
 
     def all(self):
@@ -20,14 +35,8 @@ class ModelManager:
         # TODO: raise a proper Error
 
     def filter(self, **kwargs):
-        query = q.ALL.format(self.table_name)
-        params = []
-        if kwargs:
-            query += q.WHERE
-            query += q.AND.join([q.BY_COLUMN.format(column) for column in kwargs.keys()])
-            params = tuple(kwargs.values())
         with self.db_manager as cursor:
-            cursor.execute(query, params)
+            self._execute_query_from_kwargs(cursor, **kwargs)
             records = cursor.fetchall()
             if records:
                 return [self.model_class(**record) for record in records]
